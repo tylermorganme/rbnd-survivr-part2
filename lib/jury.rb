7c76886482b0b42d2c8634e673a1d72f876e743e
Tyler <tyler@tylermorgan.me>
2016-02-28 17:24:29 -0800

    Implement Jury

---
+++ lib/jury.rb
@@ -0,0 +1,30 @@
+class Jury
+  attr_accessor :members
+  def initialize
+    @members = []
+  end
+  def add_member(member)
+    @members << member
+  end
+  def members
+    @members
+  end
+  def cast_votes(finalists)
+    final_votes = {}
+    finalists.each {|finalist| final_votes[finalist] = 0}
+    @members.each do |member|
+      vote_for = finalists.sample
+      final_votes[vote_for] +=1
+      puts "#{member} votes for #{vote_for}"
+    end
+    final_votes
+  end
+  def report_votes(final_votes)
+    final_votes.each {|finalist, votes| puts "#{finalist} received #{votes} votes"}
+  end
+  def announce_winner(final_votes)
+    final_votes.each do |finalist, votes|
+      return finalist if votes >= 4
+    end
+  end
+end
